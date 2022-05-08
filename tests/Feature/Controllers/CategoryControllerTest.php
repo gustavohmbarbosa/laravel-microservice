2758b0c2005568102a7a6dcfa5f8c940bc85e76b
Gustavo Melo <gustavobarbosa-18@hotmail.com>
2022-05-07 21:16:36 -0300

    test:  ensure CategoryController returns 404 if category not found

--- tests/Feature/Controllers/CategoryControllerTest.php
+++ tests/Feature/Controllers/CategoryControllerTest.php
@@ -7,13 +7,22 @@ use Tests\TestCase;
 
 class CategoryControllerTest extends TestCase
 {
+    const ENDPOINT = 'categories';
+
     /** @test*/
     public function return_all_categories()
     {
         Category::factory()->count(7)->create();
-        $response = $this->getJson('/categories');
+        $response = $this->getJson(self::ENDPOINT);
 
         $response->assertStatus(200);
         $response->assertJsonCount(7, 'data');
+    }
+
+    /** @test*/
+    public function should_return_404_if_category_not_found()
+    {
+        $response = $this->getJson(self::ENDPOINT . '/faker-url');
+        $response->assertStatus(404);
     }
 }
